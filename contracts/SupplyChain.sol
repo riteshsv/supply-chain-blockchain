26d8ddd8c004548b55ebe6f51a7cc9708d3e2bae
Ritesh Sangani <ritesh.sangani@gmail.com>
2024-06-20 13:58:28 +0530

    initial commit

---
+++ contracts/SupplyChain.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.0;
+
+contract SupplyChain {
+    struct Product {
+        uint256 id;
+        string name;
+        string origin;
+        string currentLocation;
+        bool isDelivered;
+    }
+
+    mapping(uint256 => Product) public products;
+    uint256 public productCounter;
+
+    event ProductAdded(uint256 id, string name, string origin);
+    event ProductShipped(uint256 id, string currentLocation);
+    event ProductDelivered(uint256 id);
+
+    function addProduct(string memory _name, string memory _origin) public {
+        productCounter++;
+        products[productCounter] = Product(productCounter, _name, _origin, _origin, false);
+        emit ProductAdded(productCounter, _name, _origin);
+    }
+
+    function shipProduct(uint256 _id, string memory _currentLocation) public {
+        Product storage product = products[_id];
+        product.currentLocation = _currentLocation;
+        emit ProductShipped(_id, _currentLocation);
+    }
+
+    function deliverProduct(uint256 _id) public {
+        Product storage product = products[_id];
+        product.isDelivered = true;
+        emit ProductDelivered(_id);
+    }
+}
